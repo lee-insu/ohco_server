f0bd1d0d52cc4d9f46ac6345085997413918e1bd
lee-insu <carpediem7760@gmail.com>
2022-03-26 16:35:58 +0900

    port

--- src/index.js
+++ src/index.js
@@ -29,20 +29,15 @@ server.applyMiddleware({
   app,
   path: "/graphql",
   cors: {
-    origin: [
-      "http://localhost:3000",
-      "https://studio.apollographql.com",
-      "https://34.82.133.56:3000",
-      "http://34.82.133.56:3000",
-    ],
+    origin: ["http://localhost:3000", "https://studio.apollographql.com"],
     credentials: true,
   },
 });
 
-await new Promise((resolve) => httpServer.listen({ port: 3000 }, resolve));
+await new Promise((resolve) => httpServer.listen({ port: 8000 }, resolve));
 
 // console.log(process.env.DB_MOMNGO);
-console.log(`🚀 Server ready at http://localhost:3000${server.graphqlPath}`);
+console.log(`🚀 Server ready at http://localhost:8000${server.graphqlPath}`);
 
 // const uri =
 //
